ba136fc958cf3ddf139bdc7f7a81ffd22a5ab18c
Yong-L <dkrcjs7521@gmail.com>
2017-11-11 15:57:58 -0500

    wip main

--- src/main.py
+++ src/main.py
@@ -1,12 +1,17 @@
 import time
 from sms import sendSMS
 from camera import takePhoto
+from watson import *
+from auth import *
 
 def main():
     try:
+        """
         while True:
             sendSMS("This is Brooklyn", "+16463715825")
             time.sleep(2)
+            """
+        post_imageV2("Trash_1268903726", "trash.jpg", ibm_auth)
     except KeyboardInterrupt:
         print("Quitting Application")
 
--- src/watson.py
+++ src/watson.py
@@ -1,13 +1,20 @@
-def post_image(url, file, api_key):
-    try:
-        url = "https://gateway-a.watsonplatform.net/visual-recognition/api/v3/classify?api_key="+api_key+"&version=2016-05-20"
+import mimetypes
+import requests
+import json
+from os.path import join, dirname
+from os import environ
+from watson_developer_cloud import VisualRecognitionV3
+
+def post_imageV2(classifier_name, file, api_key):
+    parameters={
+        "classifier_ids" : ["Trashdetector_916420994"]
+    }
+    visual_recognition = VisualRecognitionV3('2016-05-20', api_key=api_key)
 
     with open(file, 'rb') as image:
         filename = image.name
         mime_type = mimetypes.guess_type(filename)[0] or 'application/octet-stream'
         files = {'image_file': (filename, image, mime_type)}
-        r = requests.request(method = "POST", url= url, files = files)
-        print(r.text)
 
-    except:
-        print("An Error occurred uploading files")
+        print(json.dumps(visual_recognition.classify(images_file=image, 
+                classifier_ids=classifier_name), indent=2))
