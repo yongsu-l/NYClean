545c72da33012c58dd6d060d6cfa85cd33aa0f5b
Yong-L <dkrcjs7521@gmail.com>
2017-11-12 00:53:07 -0500

    Final commits

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from sms import sendSMS
 from camera import takePhoto
 from camera import initCamera
 from watson import post_imageV2
-from auth import *
+from auth import ibm_auth
 
 filename = "trash.jpg"
 
@@ -18,7 +18,7 @@ def main():
             """
             takePhoto(cam, filename)
             time.sleep(.5)
-            if post_imageV2("Trash_337459169", filename, ibm_auth):
+            if post_imageV2("TrashIdentifier_1448310972", filename, ibm_auth):
                 print("Empty the fking trash")
             else:
                 print("Don't empty trash")
--- src/training.py
+++ src/training.py
@@ -3,7 +3,14 @@ import pygame.image
 import time
 import os
 import zipfile
-from camera import *
+from camera import initCamera
+from camera import takePhoto
+import json
+from os.path import join, dirname
+from os import environ
+from watson_developer_cloud import VisualRecognitionV3
+from auth import ibm_auth
+
 
 def zipdir(path, ziph):
     # ziph is zipfile handle
@@ -23,15 +30,26 @@ def trainWatson():
         if not os.path.exists(names[i]):
             os.makedirs(names[i])
 
-        for i in range(50):
-            takePhoto(cam, names[i], names[i] + '/' + names[i] + ".jpg")
+        for j in range(50):
+            takePhoto(cam, names[i] + '/' + names[i] + str(j) + ".jpg")
 
-        confirmation = input("Read to proceed with full?: ")
 
-        while confirmation != "yes":
-            confirmation = input("Read to proceed with full?: ")
         zipf = zipfile.ZipFile(names[i] + ".zip", 'w', zipfile.ZIP_DEFLATED)
         zipdir(names[i], zipf)
         zipf.close()
+
+        if i == 0:
+            confirmation = input("Read to proceed with full?: ")
+
+            while confirmation != "yes":
+                confirmation = input("Read to proceed with full?: ")
+
+
+    visual_recognition = VisualRecognitionV3('2016-05-20', api_key=ibm_auth)
+
+    with open(join(dirname(__file__), 'full.zip'), 'rb') as full, \
+      open(join(dirname(__file__), 'empty.zip'), 'rb') as empty:
+      
+      print(json.dumps(visual_recognition.create_classifier('TrashIdentifier', _positive_examples=full, negative_examples=empty), indent=2))
 
 trainWatson()
